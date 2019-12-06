5d9fc6fbfaeb2d63a96d41aa51857babd6cb31dd
Will Charczuk <will@blend.com>
2019-12-05 21:25:17 -0800

    very complicated computer implementation.

--- solutions/2019/05/02/main.go
+++ solutions/2019/05/02/main.go
@@ -6,8 +6,38 @@ import (
 	"log"
 	"strconv"
 	"strings"
+
+	"github.com/wcharczuk/advent/pkg/intcode"
 )
 
+func main() {
+	contents, err := ioutil.ReadFile("../input")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	rawValues := strings.Split(string(contents), ",")
+	opCodes := make([]int, len(rawValues))
+
+	for x := 0; x < len(rawValues); x++ {
+		opCodes[x], err = strconv.Atoi(strings.TrimSpace(rawValues[x]))
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	computer := intcode.New(opCodes...)
+
+	err = computer.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, logItem := range computer.Log {
+		fmt.Println(logItem.String())
+	}
+}
+
+/*
 func main() {
 	contents, err := ioutil.ReadFile("../input")
 	if err != nil {
@@ -168,3 +198,4 @@ func readInt() int {
 	fmt.Scanf("%d", &i)
 	return i
 }
+*/
